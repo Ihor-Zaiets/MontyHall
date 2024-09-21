1ad2f81217692a43c74fe35dbf270335f424c035
Ihor Zaiets <dekstroer8202@gmail.com>
2024-09-21 11:34:46 +0200

    refactor code

--- src/main/java/com/ihorzaiets/MontyHall.java
+++ src/main/java/com/ihorzaiets/MontyHall.java
@@ -1,68 +1,73 @@
 package com.ihorzaiets;
 
+import java.text.NumberFormat;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class MontyHall {
 
-    private static HashMap<Integer, Boolean> doorPriceMap = new HashMap<>();
+    private static final boolean DO_PLAYER_SWITCH_CHOICE = true;
+    private static final int NUMBER_OF_SIMULATIONS = 10000000;
+
+    private static HashMap<Integer, Boolean> doorPrizeMap = new HashMap<>();
     private static int playerDoorChoice;
     private static int winningDoor;
     private static boolean isPlayerWinning;
-    private static float percentageOfWin;
 
     public static void main(String[] args) {
-        runSimulationForMontyHall(3);
-        runSimulationForMontyHall(100);
+        runMontyHallSimulation(3);
+        runMontyHallSimulation(100);
     }
 
-    private static void runSimulationForMontyHall(int numberOfDoors) {
-        System.out.printf("Running simulation for %s doors.\n", numberOfDoors);
-        int numberOfSimulations = 10000000;
+    private static void runMontyHallSimulation(int numberOfDoors) {
+        String formattedNumberOfSimulations = NumberFormat.getInstance(Locale.FRANCE).format(NUMBER_OF_SIMULATIONS);
+        System.out.printf("Running %s Monty Hall simulations for %s doors.\n", formattedNumberOfSimulations, numberOfDoors);
         int numberOfWins = 0;
-        for (int i = 0; i < numberOfSimulations; i++) {
+        for (int i = 0; i < NUMBER_OF_SIMULATIONS; i++) {
             createDoors(numberOfDoors);
             pickADoor();
             revealWrongDoors();
             offerChoiceSwitch();
             if (isPlayerWinning) numberOfWins++;
         }
-        percentageOfWin = (float)numberOfWins / (float)numberOfSimulations * 100;
+        float percentageOfWin = (float) numberOfWins / (float) NUMBER_OF_SIMULATIONS * 100;
         System.out.printf("Player won in %s%% of simulations.\n", percentageOfWin);
     }
 
-
-
-    private static void offerChoiceSwitch() {
-        //always switch
-        isPlayerWinning = !doorPriceMap.get(playerDoorChoice);
-    }
-
-    private static void revealWrongDoors() {
-        int secondDoor = (int) (Math.random() * doorPriceMap.size()) + 1;
-        while (secondDoor == playerDoorChoice) {
-            secondDoor = (int)(Math.random() * doorPriceMap.size()) + 1;
+    private static void createDoors(int numberOfDoors) {
+        for (int i = 1; i <= numberOfDoors; i++) {
+            doorPrizeMap.put(i, false);
         }
-        if (doorPriceMap.get(playerDoorChoice)) {
-            doorPriceMap = new HashMap<>();
-            doorPriceMap.put(playerDoorChoice, true);
-            doorPriceMap.put(secondDoor, false);
-        } else {
-            doorPriceMap = new HashMap<>();
-            doorPriceMap.put(playerDoorChoice, false);
-            doorPriceMap.put(winningDoor, true);
-        }
+        winningDoor = pickRandomDoor();
+        doorPrizeMap.replace(winningDoor, true);
     }
 
     private static void pickADoor() {
-        playerDoorChoice = (int)(Math.random() * doorPriceMap.size()) + 1;
+        playerDoorChoice = pickRandomDoor();
     }
 
-    private static void createDoors(int numberOfDoors) {
-
-        for (int i = 1; i <= numberOfDoors; i++) {
-            doorPriceMap.put(i, false);
+    private static void revealWrongDoors() {
+        int secondDoor = pickRandomDoor();
+        while (secondDoor == playerDoorChoice) {
+            secondDoor = pickRandomDoor();
         }
-        winningDoor = (int)(Math.random() * numberOfDoors) + 1;
-        doorPriceMap.replace(winningDoor, true);
+        if (doorPrizeMap.get(playerDoorChoice)) {
+            doorPrizeMap = new HashMap<>();
+            doorPrizeMap.put(playerDoorChoice, true);
+            doorPrizeMap.put(secondDoor, false);
+        } else {
+            doorPrizeMap = new HashMap<>();
+            doorPrizeMap.put(playerDoorChoice, false);
+            doorPrizeMap.put(winningDoor, true);
+        }
+    }
+
+    private static void offerChoiceSwitch() {
+        isPlayerWinning = doorPrizeMap.get(playerDoorChoice);
+        if (DO_PLAYER_SWITCH_CHOICE) isPlayerWinning = !isPlayerWinning;
+    }
+
+    private static int pickRandomDoor() {
+        return (int) (Math.random() * doorPrizeMap.size()) + 1;
     }
 }
